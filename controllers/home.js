4777b2a81a1b7a868f0227d426eccaa6347e98c3
Popey Gilbert <popey@debenclipper.com>
2016-11-15 10:26:54 +0000

    Fix cookie

--- controllers/home.js
+++ controllers/home.js
@@ -247,7 +247,6 @@ function toMarkdownData(doc) {
  */
 exports.postEditSequence = function(req, res) {
   console.log("Hey: " + req.body)
-  console.log("...?")
   console.log(req.body)
   res.send("Hey")
   // res.json({name: "John", time: "2pm"})
---
+++ public/js/cookie.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#efefef",
+        "text": "#404040"
+      },
+      "button": {
+        "background": "#8ec760",
+        "text": "#ffffff"
+      }
+    },
+    "showLink": false,
+    "theme": "classic"
+  })
+});
